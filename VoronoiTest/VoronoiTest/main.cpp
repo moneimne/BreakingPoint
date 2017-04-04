1686f7ca25235d917b7eb0cf91b3ca383de7c3c0
Mohamad Moneimne <https://moneimne@bitbucket.org>
2017-04-04 18:49:27 -0400

    Finished voronoi parse function

--- VoronoiTest/VoronoiTest/main.cpp
+++ VoronoiTest/VoronoiTest/main.cpp
@@ -22,16 +22,84 @@ const int particles = 3;
 // This function returns a random double between 0 and 1
 double rnd() { return double(rand()) / RAND_MAX; }
 
-int main() {
-	//testVoro();
-}
-
 void testBoolean() {
 
 }
 
-void testParse() {
+std::vector<std::pair<std::vector<std::vector<float>>, std::vector<std::vector<int>>>> testParse(std::string inputFile) {
+	std::vector<std::pair<std::vector<std::vector<float>>, std::vector<std::vector<int>>>> voroData = std::vector<std::pair<std::vector<std::vector<float>>, std::vector<std::vector<int>>>>();
+	
+	FILE *voroFile = fopen(inputFile.c_str(), "r");
+	if (voroFile == NULL) {
+		printf("IOError: %s couldn't be opened.\n", inputFile.c_str());
+		return voroData;
+	}
 
+	char line[1000];
+	
+	fgets(line, 1000, voroFile);
+	while (true) {
+		// Get number of vertices
+		int numVertices;
+		sscanf(line, "%i", &numVertices);
+
+		// Get vertices
+		std::vector<std::vector<float>> vertexData = std::vector<std::vector<float>>();
+		fgets(line, 1000, voroFile);
+		int numChars = 0;
+		for (int i = 0; i < numVertices; i++) {
+			float x, y, z;
+			int numCharsRead;
+			sscanf(line + numChars, "(%f,%f,%f) %n", &x, &y, &z, &numCharsRead);
+			numChars += numCharsRead;
+
+			std::vector<float> vertex = std::vector<float>();
+			vertex.push_back(x);
+			vertex.push_back(y);
+			vertex.push_back(z);
+
+			vertexData.push_back(vertex);
+		}
+
+		// Get number of faces
+		int numFaces;
+		fgets(line, 1000, voroFile);
+		sscanf(line, "%i", &numFaces);
+
+		// Get face indices
+		std::vector<std::vector<int>> faceData = std::vector<std::vector<int>>();
+		fgets(line, 1000, voroFile);
+		numChars = 0;
+		for (int i = 0; i < numFaces; i++) {
+			// Get face data
+			char face[1000];
+			int numCharsRead;
+			sscanf(line + numChars, "%s %n", &face, &numCharsRead);
+			numChars += numCharsRead;
+
+			// Save indices
+			int numIndices = (numCharsRead / 2) - 1;
+			int numFaceChars = 0;
+			std::vector<int> vertexIndices = std::vector<int>();
+			for (int j = 0; j < numIndices; j++) {
+				int f;
+				sscanf(face + numFaceChars + 1, "%i", &f);
+				numFaceChars += 2;
+				vertexIndices.push_back(f);
+			}
+
+			faceData.push_back(vertexIndices);
+		}
+
+		std::pair<std::vector<std::vector<float>>, std::vector<std::vector<int>>> pairData = std::pair<std::vector<std::vector<float>>, std::vector<std::vector<int>>>(vertexData, faceData);
+		voroData.push_back(pairData);
+
+		// Termination
+		if (!fgets(line, 1000, voroFile)) {
+			break;
+		}
+	}
+	return voroData;
 }
 
 void testVoro() {
@@ -52,7 +120,7 @@ void testVoro() {
 		con.put(i, x, y, z);
 	}
 
-	con.print_custom("%i\n%P\n%t", "output.txt");
+	con.print_custom("%w\n%P\n%s\n%t", "output.txt");
 
 	// Sum up the volumes, and check that this matches the container volume
 	double vvol = con.sum_cell_volumes();
@@ -65,4 +133,10 @@ void testVoro() {
 
 	// Output the Voronoi cells in gnuplot format
 	con.draw_cells_gnuplot("random_points_v.gnu");
+}
+
+int main() {
+	testVoro();
+	std::vector<std::pair<std::vector<std::vector<float>>, std::vector<std::vector<int>>>> voroData = testParse("output.txt");
+	printf("done!\n");
 }
